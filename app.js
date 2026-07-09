c9998c2263808dc7709423201fbd7f62cf058d4c
AcousticKult <45168150+AcousticKult@users.noreply.github.com>
2026-07-09 01:51:26 +0000

    Fix square grid JS and CSS Flexbox

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const container = document.getElementById("container");
 container.style.width = "800px";
 container.style.height = "800px";
 
-const userNumber = "16";
+const userNumber = 16;
 
-for (let i =  1; i < (userNumber ** 2); i++) {
+for (let i =  0; i < (userNumber ** 2); i++) {
     const squares = document.createElement("div");
-    squares.style.width = container.style.width / userNumber;
-    squares.style.height = squares.style.width;
+    squares.style.width = `${parseInt(container.style.width)/userNumber}px`;
+    squares.style.height = `${parseInt(container.style.height)/userNumber}px`;
     // squares.classList.add("squares");
     container.append(squares);
-
-    /*;
-        squareRow.style.height = "50px";
-        squareRow.classList.add("squareRow");
-        container.append(squareRow);
-    }*/
 }
 
 const button = document.querySelector("button");
