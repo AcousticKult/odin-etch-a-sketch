b080ba997a7ae8fa71064769ceea341ab8312a8f
AcousticKult <45168150+AcousticKult@users.noreply.github.com>
2026-07-15 12:02:49 -0400

    Add hover class

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ for (let i =  0; i < (userNumber ** 2); i++) {
     const squares = document.createElement("div");
     squares.style.width = `${parseInt(container.style.width)/userNumber}px`;
     squares.style.height = `${parseInt(container.style.height)/userNumber}px`;
-    console.log(squares.style.width);
     container.append(squares);
 }
 
-squares.addEventListener("onmouseenter", () => {
-    squares.classList.add(newClass);
-});
+for (const child of container.children) {
+    child.addEventListener("mouseenter", () => {
+        child.classList.add("newClass");
+    });
+}
 
 /* const button = document.querySelector("button");
 
