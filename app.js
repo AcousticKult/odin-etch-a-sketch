bc70643ae7804e3c715002d366acd30a7d9dd5ce
AcousticKult <45168150+AcousticKult@users.noreply.github.com>
2026-07-16 16:03:07 +0000

    Add button functionality

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
+// create container with defined dimensions
 const container = document.getElementById("container");
 container.style.width = "800px";
 container.style.height = "800px";
 
+// create initial square size + highlight colour
 let userNumber = 16;
 
-for (let i =  0; i < (userNumber ** 2); i++) {
-    const squares = document.createElement("div");
-    squares.style.width = `${parseInt(container.style.width)/userNumber}px`;
-    squares.style.height = `${parseInt(container.style.height)/userNumber}px`;
-    container.append(squares);
+createSquares(userNumber);
+addClassList();
+
+function createSquares(userNumber) {
+    for (let i =  0; i < (userNumber ** 2); i++) {
+        const squares = document.createElement("div");
+        squares.style.width = `${parseInt(container.style.width)/userNumber}px`;
+        squares.style.height = `${parseInt(container.style.height)/userNumber}px`;
+        container.append(squares);
+    };
 };
 
-for (const child of container.children) {
-    child.addEventListener("mouseenter", () => {
-        child.classList.add("newClass");
-    });
+function addClassList() {
+    for (const child of container.children) {
+        child.addEventListener("mouseenter", () => {
+            child.classList.add("newClass");
+        });
+    };
 };
 
 const button = document.querySelector("button");
-
 button.addEventListener("click", handleButtonClick);
+
+let result = document.createElement("p");
 
 function handleButtonClick() {
     let userInput = prompt("Give me a number between 1 - 100.");
 
-    const result = document.createElement("p");
-
     if (userInput === null) {
-        result.innerText = "OK, maybe next time.";
+        result.textContent = "OK, maybe next time.";
         buttonDiv.append(result);
     } else if (userInput.toLowerCase() === "") {
-        result.innerText = "Don't be shy, enter a number!";
+        result.textContent = "Don't be shy, enter a number!";
         buttonDiv.append(result);
-    } else if (userInput < 0 || userInput > 100) {
-        result.innerText = "Please choose a number between 1 to 100 only!";
+    } else if (userInput < 1 || userInput > 100) {
+        result.textContent = "Please choose a number between 1 to 100 only!";
         buttonDiv.append(result);
     } else {
-        userNumber = userInput;
+        userNumber = parseInt(userInput);
+
+        container.replaceChildren();
+        createSquares(userNumber);
+        addClassList();
+        
+        result.textContent = "";
     }
+
+    this.blur();
 };
